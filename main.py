cedfc287665676b643dfef1da0bf79c5f91d42bc
thain <thai.nguyen18@gmail.com>
2017-06-30 21:10:13 -0400

    changed the way potential was calculated (doesn't go near boundaries) and added contour lines to plot

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.linalg
 
 if __name__ == '__main__':
     # Parameters
-    max_iterations = int(1e3)
+    max_iterations = 10000
+    tolerance = 1e-3
 
     # Define dimensions of simulation space
     h = 1
@@ -14,24 +16,33 @@ if __name__ == '__main__':
     sim_space = np.meshgrid(x, y)
 
     # Allocate memory for storing potential function and give initial values
-    V = 50 * np.ones((np.size(x), np.size(y)))
+    V = np.zeros((np.size(x), np.size(y)))
+
+    # Apply voltage at electrodes
+    V[11:111, 0] = 100
+    V[0, 1:122] = 0
+    V[121:122, 1:122] = 0
+    V[1:121, 101:102] = 0
 
     # Calculate
     for iter in range(max_iterations):
-        for i in range(np.size(x) - 1):
-            for j in range(np.size(y) - 1):
-                # Maintain voltage at electrodes
-                V[11:111, 0:1] = 100
-                V[0:1, 1:122] = 0
-                V[121:122, 1:122] = 0
-                V[1:121, 101:102] = 0
+        oldV = V.copy()
+        for i in range(1, np.size(x) - 1):
+            for j in range(1, np.size(y) - 1):
                 # Traverse mesh and update
                 V[i, j] = (V[i+1, j] + V[i-1, j] + V[i, j+1] + V[i, j-1]) / 4
-        print('Iteration:', iter)
+        # Check if converged
+        err = np.max(numpy.abs((V[1:x.size-2, 1:y.size-2] - oldV[1:x.size-2, 1:y.size-2])/V[1:x.size-2, 1:y.size-2]))
+        print('Iteration:', iter, 'Error:', err)
+        if err < tolerance:
+            print('Solution converged')
+            break
 
     V = np.transpose(V)
-    plt.contourf(x, y, V)
-    cs = plt.pcolor(V)
-    plt.colorbar(cs)
+    levels = np.arange(0, 100, 5)
+    cs = plt.contourf(x, y, V, levels, origin='upper')
+    cs2 = plt.contour(cs, colors='k', origin='upper')
+    cbar = plt.colorbar(cs)
+    cbar.add_lines(cs2)
     plt.savefig('potential_plot.png')
     plt.show()
